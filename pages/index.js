00cb50ba40bd8a7ff84cc61dddff338312de82b0
moses <chchxun@gmail.com>
2025-12-29 23:23:12 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>华人教会四十年歌与梦</h1>
+
+      <blockquote>
+        申8:2-6  
+        你也要记念耶和华你的神在旷野引导你，这四十年……
+      </blockquote>
+
+      <h2>关于本网站</h2>
+      <p>
+        本站为华人教会四十年历史资料整理与见证汇编。
+        内容为作者多年写作的最终版本，
+        用于存档、查阅、分辨与警醒。
+      </p>
+
+      <h2>浏览指南</h2>
+      <ul>
+        <li>若时间有限，只需阅读三篇总纲信息</li>
+        <li>不服的，再逐条查证</li>
+        <li>本站无人值守，不提供互动</li>
+      </ul>
+
+      <h2>核心文章</h2>
+      <ul>
+        <li>
+          <a href="https://wp.me/pcR1p0-v5">
+            《倪柝声为中国教会贡献了什么》
+          </a>
+        </li>
+        <li>
+          <a href="https://wp.me/pcR1p0-o6">
+            《认识灵恩派》
+          </a>
+        </li>
+        <li>
+          <a href="https://wp.me/pcR1p0-WO">
+            《唐崇荣牧师遗训》
+          </a>
+        </li>
+      </ul>
+    </main>
+  );
+}
